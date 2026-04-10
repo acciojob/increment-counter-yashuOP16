aa27a649b3ad521662c8d82a0ad7385088ad456b
Yash <132873361+yashuOP16@users.noreply.github.com>
2026-04-10 14:58:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//your JS code here. If required.
+let count = 0;
+
+const counterEl = document.getElementById("counter");
+const btn = document.getElementById("incrementBtn");
+
+btn.addEventListener("click", () => {
+    alert(count);
+
+    count++;
+
+    counterEl.innerText = count;
+});
